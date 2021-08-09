8dd420b2b8112565702e5ee8ac8e21c276544953
Audren Bouëssel du Bourg <audren.bdb@gmail.com>
2021-08-09 12:00:34 +0200

    doc

--- locale.go
+++ locale.go
@@ -6,6 +6,8 @@ const (
 	defaultLang = "en"
 )
 
+//GetLang returns first two letters of runtime language
+//or "en" if not found / error.
 func GetLang() string {
 	l := getOSLocale()
 	l = strings.TrimSpace(l)
